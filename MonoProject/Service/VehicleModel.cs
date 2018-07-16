be4ce91ca46264e0bb1f282f7244222f6a7e0064
Ivan <ibudisa11@gmail.com>
2018-07-16 18:33:41 +0200

    2
    Dodan je code first pristup bazi

---
+++ MonoProject/Service/VehicleModel.cs
@@ -0,0 +1,24 @@
+namespace Service
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("VehicleModel")]
+    public partial class VehicleModel
+    {
+        public int Id { get; set; }
+
+        public int? MakeId { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Abrv { get; set; }
+
+        public virtual VehicleMake VehicleMake { get; set; }
+    }
+}
